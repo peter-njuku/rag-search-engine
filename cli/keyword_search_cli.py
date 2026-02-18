8d87c331e06a1b446da68a5b0ddb3a6387c2bbf1
peter-njuku <137099178+peter-njuku@users.noreply.github.com>
2026-02-18 11:17:41 +0000

    Keyword search cli

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Key word Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parsers = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parsers.add_argument("query", type=str, help="Search query")
+    
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            pass
+
+        case _:
+            parser.print_help()
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,1 @@
+print("Hello from rag-search-engine!")
