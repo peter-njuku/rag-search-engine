994d048cfebeb5e9b7e1072539e01d1270d538c8
peter-njuku <njuku2005@gmail.com>
2026-05-12 12:40:43 +0300

    Text Embedding

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -5,11 +5,27 @@ class SemanticSearch:
     def __init__(self, ):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
 
+    def generate_embedding(self, text):
+        if not isinstance(text, str):
+            raise ValueError("[Error] - Input text must be a string.")
+        if not text.strip():
+            raise ValueError("[Error] - Input text cannot be empty or only whitespace.")
+
+        embedding_list = self.model.encode([text])
+        return embedding_list[0]
+
 
 def verify_model():
     semantic_search = SemanticSearch()
     print(f"Model loaded successfully: {semantic_search.model}")
     print("Max sequence length:", semantic_search.model.max_seq_length)
+
+def embed_text(text):
+    semantic_search = SemanticSearch()
+    embedding = semantic_search.generate_embedding(text)   
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 if __name__ == "__main__":
     verify_model()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from cli.lib.semantic_search import verify_model
+from cli.lib.semantic_search import embed_text, verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -10,11 +10,20 @@ def main():
     
     verify_parser = subparsers.add_parser("verify", help="Verify that the semantic search model loads correctly")
     
+    embed_parser = subparsers.add_parser("embed_text", help="Generate an embedding for a given text")
+    embed_parser.add_argument("text", type=str, help="The text to generate an embedding for")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed_text":
+            if not args.text:
+                parser.error("the 'text' argument is required for the 'embed_text' command")
+            embed_text(args.text)
+
         case _:
             parser.print_help()
 
